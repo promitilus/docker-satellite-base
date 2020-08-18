5ccd4281a85f862a31d27f51984dcb24659ef4e7
Samuel Behan <samuel.behan@dob.sk>
2020-08-18 12:03:19 +0200

    add reconfigure service

--- files/etc/boot.d/60_tunnel.sh
+++ files/etc/boot.d/60_tunnel.sh
@@ -2,5 +2,5 @@
 
 echo "RR_SSH_HOSTS=$SATELLITE_REMOTE_HOST" >/etc/environment.tunnel
 
-systemctl enable ssh-tunnel \
-	&& ( systemctl start ssh-tunnel || true)
+systemctl enable ssh-tunnel ssh-tunnel-reconfigure.service ssh-tunnel-reconfigure.timer \
+	&& ( systemctl start ssh-tunnel ssh-tunnel-reconfigure.timer || true)
